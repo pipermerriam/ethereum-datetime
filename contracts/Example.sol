eb7d84eb472289ecc166fb554daaf524bf18d2ef
Piper Merriam <pipermerriam@gmail.com>
2015-10-06 10:48:50 -0600

    remove example contract, add API contract

--- contracts/Example.sol
+++
@@ -1,4 +0,0 @@
-contract Example {
-        function Example() {
-        }
-}
---
+++ contracts/api.sol
@@ -0,0 +1,19 @@
+contract DateTimeAPI {
+        /*
+         *  Abstract contract for interfacing with the DateTime contract.
+         *
+         */
+        function isLeapYear(uint16 year) constant returns (bool);
+        function getYear(uint timestamp) constant returns (uint16);
+        function getMonth(uint timestamp) constant returns (uint16);
+        function getDay(uint timestamp) constant returns (uint16);
+        function getHour(uint timestamp) constant returns (uint16);
+        function getMinute(uint timestamp) constant returns (uint16);
+        function getSecond(uint timestamp) constant returns (uint16);
+        function getWeekday(uint timestamp) constant returns (uint8);
+        function toTimestamp(uint16 year, uint8 month, uint8 day) constant returns (uint timestamp);
+        function toTimestamp(uint16 year, uint8 month, uint8 day, uint8 hour) constant returns (uint timestamp);
+        function toTimestamp(uint16 year, uint8 month, uint8 day, uint8 hour, uint8 minute) constant returns (uint timestamp);
+        function toTimestamp(uint16 year, uint8 month, uint8 day, uint8 hour, uint8 minute, uint8 second) constant returns (uint timestamp);
+}
+
