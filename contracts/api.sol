82ee3bcd6e4d1e1afec46f85f53666448a76405c
Unknown <dmitry537@gmail.com>
2018-10-15 18:09:33 +0300

    added sum & sub functions

--- contracts/api.sol
+++ contracts/api.sol
@@ -15,5 +15,11 @@ contract DateTimeAPI {
         function toTimestamp(uint16 year, uint8 month, uint8 day, uint8 hour) constant returns (uint timestamp);
         function toTimestamp(uint16 year, uint8 month, uint8 day, uint8 hour, uint8 minute) constant returns (uint timestamp);
         function toTimestamp(uint16 year, uint8 month, uint8 day, uint8 hour, uint8 minute, uint8 second) constant returns (uint timestamp);
+        function addYears(uint timestamp, uint16 addingYears) public pure returns (uint);
+        function addMonths(uint timestamp, uint16 addingMonths) public pure returns (uint);
+        function addDays(uint timestamp, uint16 addingDays) public pure returns (uint);
+        function subYears(uint timestamp, uint16 subtractingYears) public pure returns (uint);
+        function subMonths(uint timestamp, uint16 subtractingMonths) public pure returns (uint);
+        function subDays(uint timestamp, uint16 subtractingDays) public pure returns (uint);
 }
 
